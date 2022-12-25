60b62fae7b989d7cc7cc7906a4773eee6a8a7030
Kochneva Kristina <103591010+Ryrykia@users.noreply.github.com>
2022-12-25 14:46:06 +0500

    Add files via upload

---
+++ Практика 3/Wp1/Wp6/Wp6/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wp6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public class Router
+        {
+            public string Name { get; set; }
+            public string Vendor { get; set; }
+            public string Type { get; set; }
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            List<Router> routers = new List<Router>
+            {
+            new Router{Name="CSR1000V", Vendor="Cisco", Type="Virtual"},
+            new Router{Name="1921", Vendor="Cisco", Type="Hardware"},
+            };
+            dataGrid.ItemsSource = routers;
+        }
+    }
+}
