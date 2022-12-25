4074479404c6ce57f162aece9c82eb9a6b87624a
Kochneva Kristina <103591010+Ryrykia@users.noreply.github.com>
2022-12-25 14:48:42 +0500

    Add files via upload

---
+++ Практика 4/Wp1/Wp1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        class Min
+        {
+            int[] ar = { 1, 4, 5, 12, 18, 20};
+            public int Rs()
+            {
+                var ans = from element in ar
+                          where element > 0
+                          select element;
+                return (ans.Count() > 0 ? ans.Min() : 0);
+            }
+        }
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Min min = new Min();
+            lab1.Content = min.Rs();
+        }
+    }
+}
